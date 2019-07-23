743a68e17a019cfb60d4839f6fd5cd720305ce22
shalin007 <34646376+shalin007@users.noreply.github.com>
2019-07-23 23:40:33 +0530

    finally adding staic files

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -149,7 +149,7 @@ STATICFILES_DIR=[
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),'static_cd',"static_root")
+STATIC_ROOT=os.path.join(BASE_DIR,'static_cd',"static_root")
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'static_cd',"media_root")
